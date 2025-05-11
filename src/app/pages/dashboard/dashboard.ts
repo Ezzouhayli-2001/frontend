5a3659f624a06a45166c7ef3f8d4ea17d4e231e4
YoussafOuhba <youssefouhba@gmail.com>
2025-05-11 12:10:11 +0100

    first commit

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -11,7 +11,7 @@ import { RevenueStreamWidget } from './components/revenuestreamwidget';
     standalone: true,
     template: `
         <div class="grid grid-cols-12 gap-8">
-            <app-stats-widget class="contents"/>
+           <!-- <app-stats-widget class="contents"/>
             <div class="col-span-12 xl:col-span-6">
                 <app-recent-sales-widget/>
                 <app-best-selling-widget/>
@@ -19,7 +19,7 @@ import { RevenueStreamWidget } from './components/revenuestreamwidget';
             <div class="col-span-12 xl:col-span-6">
                 <app-revenue-stream-widget/>
                 <app-notifications-widget/>
-            </div>
+            </div>-->
         </div>
     `
 })
